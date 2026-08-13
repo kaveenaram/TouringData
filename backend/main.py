77600149c8133b246858566e828738ffcc63e7ea
kaveenaram <kaveenaram@outlook.com>
2026-08-13 13:56:13 -0400

    first commit

---
+++ backend/main.py
@@ -0,0 +1,4 @@
+# basically i am creating a website that uses spotify api and bandsintown data to determine
+# when is the best time to have an artist tour
+# whether a city is a wise tour stop
+# and to find venues in the city that can hold a capacity good enough for the artist
