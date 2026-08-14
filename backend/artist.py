65f008e4626104082708953c3208338ab2c67827
kaveenaram <kaveenaram@outlook.com>
2026-08-13 20:43:11 -0400

    beginning to add artist methods, created dashboard function, and created searchArtist function

--- backend/artist.py
+++ backend/artist.py
@@ -1,2 +1,12 @@
 # should have all methods for getting artist data from soundcharts api
 # minimize API calls
+
+
+from api import sc
+
+def searchArtist(input: str):
+# input will always be a string because we are getting it from the front end
+    artist = sc.search_artist_by_name(input, offset=0, limit=1)
+    if artist is None:
+        return "Try Again"
+    return artist
--- backend/main.py
+++ backend/main.py
@@ -8,3 +8,11 @@ def dashboard():
         # if artist does not exist, pop up error and try again
     # second: once artist is found, dashboard should automatically ask for information from API based on artist and city
     # find ways in backend to minimize API calls
+
+    # ex / puesdo code
+    # input = "Jae Stephens"
+    # THIS WOULD NEED TO BE IN ARTIST.PY TO HIDE THE TRY AGAIN PART
+    # artist = json.loads(sc.search_for_artist(input, offset=0, limit=1))
+    # if artist = None: "Try Again" --> searchMethod
+    # else artist_uuid = artist["uuid"] 
+    # now you would use this uuid to access the rest of the info through the artist.py methods
